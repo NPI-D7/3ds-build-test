bf8fc63d9e109eea10633efc1fa9e9b2a2c1f217
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-09-23 15:10:31 +0200

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -1,32 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <3ds.h>
+/*
+*   This file is part of Universal-Core-Example
+*   Copyright (C) 2020 SuperSaiyajinStackie
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Additional Terms 7.b and 7.c of GPLv3 apply to this file:
+*       * Requiring preservation of specified reasonable legal notices or
+*         author attributions in that material or in the Appropriate Legal
+*         Notices displayed by works containing it.
+*       * Prohibiting misrepresentation of the origin of that material,
+*         or requiring that modified versions of such material be marked in
+*         reasonable ways as different from the original version.
+*/
 
-int main(int argc, char* argv[])
-{
-	gfxInitDefault();
-	consoleInit(GFX_TOP, NULL);
-//print
-	printf("Heellllo!\n");
+#include "init.hpp"
 
-	// Main loop
-	while (aptMainLoop())
-	{
-		gspWaitForVBlank();
-		gfxSwapBuffers();
-		hidScanInput();
-
-		// Your code goes here
-		u32 kDown = hidKeysDown();
-		if (kDown & KEY_START){
-			
-			break; // break in order to return to hbmenu
-			
-	}
-	
-
-	gfxExit();
-	return 0;
-}
-} 
+int main() { Init::MainLoop(); }
