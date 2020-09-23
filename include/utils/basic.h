b1aa2ddc2fac555d41df8dd4c1b576d66893d0f1
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-09-23 20:31:03 +0200

    Add files via upload

---
+++ include/utils/basic.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <3ds.h>
+
+#define API_URL "https://api.github.com/repos/AuroraWright/Luma3DS/releases/latest"
+#define ARCHIVE_FILE_PATH "/latest_luma_release.7z"
