32b72219b1fb502bfea59fc34499016feb0d3d4c
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-09-24 12:46:57 +0200

    Add files via upload

---
+++ source/utils/download.cpp
@@ -0,0 +1,229 @@
+#include "download.hpp"
+
+#define  USER_AGENT   APP_TITLE "-" VERSION_STRING
+
+static char* result_buf = NULL;
+static size_t result_sz = 0;
+static size_t result_written = 0;
+
+// following function is from 
+// https://github.com/angelsl/libctrfgh/blob/master/curl_test/src/main.c
+static size_t handle_data(char* ptr, size_t size, size_t nmemb, void* userdata)
+{
+    (void) userdata;
+    const size_t bsz = size*nmemb;
+
+    if (result_sz == 0 || !result_buf)
+    {
+        result_sz = 0x1000;
+        result_buf = (char*)malloc(result_sz);
+    }
+
+    bool need_realloc = false;
+    while (result_written + bsz > result_sz) 
+    {
+        result_sz <<= 1;
+        need_realloc = true;
+    }
+
+    if (need_realloc)
+    {
+        char *new_buf = (char*)realloc(result_buf, result_sz);
+        if (!new_buf)
+        {
+            return 0;
+        }
+        result_buf = new_buf;
+    }
+
+    if (!result_buf)
+    {
+        return 0;
+    }
+
+    memcpy(result_buf + result_written, ptr, bsz);
+    result_written += bsz;
+    return bsz;
+}
+
+static Result setupContext(CURL *hnd, const char * url)
+{
+    curl_easy_setopt(hnd, CURLOPT_BUFFERSIZE, 102400L);
+    curl_easy_setopt(hnd, CURLOPT_URL, url);
+    curl_easy_setopt(hnd, CURLOPT_NOPROGRESS, 1L);
+    curl_easy_setopt(hnd, CURLOPT_USERAGENT, USER_AGENT);
+    curl_easy_setopt(hnd, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(hnd, CURLOPT_MAXREDIRS, 50L);
+    curl_easy_setopt(hnd, CURLOPT_HTTP_VERSION, (long)CURL_HTTP_VERSION_2TLS);
+    curl_easy_setopt(hnd, CURLOPT_WRITEFUNCTION, handle_data);
+    curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(hnd, CURLOPT_VERBOSE, 1L);
+    curl_easy_setopt(hnd, CURLOPT_STDERR, stdout);
+
+    return 0;
+}
+
+Result downloadToFile(std::string url, std::string path)
+{
+	Result ret = 0;	
+	printf("Downloading from:\n%s\nto:\n%s\n", url.c_str(), path.c_str());
+
+    void *socubuf = memalign(0x1000, 0x100000);
+    if (!socubuf)
+    {
+        return -1;
+    }
+
+	ret = socInit((u32*)socubuf, 0x100000);
+	if (R_FAILED(ret))
+    {
+		free(socubuf);
+        return ret;
+    }
+
+	CURL *hnd = curl_easy_init();
+	ret = setupContext(hnd, url.c_str());
+	if (ret != 0) {
+		socExit();
+		free(result_buf);
+		free(socubuf);
+		result_buf = NULL;
+		result_sz = 0;
+		result_written = 0;
+		return ret;
+	}
+
+	Handle fileHandle;
+	u64 offset = 0;
+	u32 bytesWritten = 0;
+	
+	ret = openFile(&fileHandle, path.c_str(), true);
+	if (R_FAILED(ret)) {
+		printf("Error: couldn't open file to write.\n");
+		socExit();
+		free(result_buf);
+		free(socubuf);
+		result_buf = NULL;
+		result_sz = 0;
+		result_written = 0;
+		return DL_ERROR_WRITEFILE;
+	}
+	
+	u64 startTime = osGetTime();
+
+	CURLcode cres = curl_easy_perform(hnd);
+    curl_easy_cleanup(hnd);
+
+	if (cres != CURLE_OK) {
+		printf("Error in:\ncurl\n");
+		socExit();
+		free(result_buf);
+		free(socubuf);
+		result_buf = NULL;
+		result_sz = 0;
+		result_written = 0;
+		return -1;
+	}
+	
+	FSFILE_Write(fileHandle, &bytesWritten, offset, result_buf, result_written, 0);
+
+	u64 endTime = osGetTime();
+	u64 totalTime = endTime - startTime;
+	printf("Download took %llu milliseconds.\n", totalTime);
+
+    socExit();
+    free(result_buf);
+    free(socubuf);
+	result_buf = NULL;
+	result_sz = 0;
+	result_written = 0;
+	FSFILE_Close(fileHandle);
+	return 0;
+}
+
+Result downloadFromRelease(std::string url, std::string asset, std::string path)
+{
+	Result ret = 0;
+    void *socubuf = memalign(0x1000, 0x100000);
+    if (!socubuf)
+    {
+        return -1;
+    }
+
+	ret = socInit((u32*)socubuf, 0x100000);
+	if (R_FAILED(ret))
+    {
+		free(socubuf);
+        return ret;
+    }
+
+	std::regex parseUrl("github\\.com\\/(.+)\\/(.+)");
+	std::smatch result;
+	regex_search(url, result, parseUrl);
+	
+	std::string repoOwner = result[1].str(), repoName = result[2].str();
+	
+	std::stringstream apiurlStream;
+	apiurlStream << "https://api.github.com/repos/" << repoOwner << "/" << repoName << "/releases/latest";
+	std::string apiurl = apiurlStream.str();
+	
+	printf("Downloading latest release from repo:\n%s\nby:\n%s\n", repoName.c_str(), repoOwner.c_str());
+	printf("Crafted API url:\n%s\n", apiurl.c_str());
+	
+	CURL *hnd = curl_easy_init();
+	ret = setupContext(hnd, apiurl.c_str());
+	if (ret != 0) {
+		socExit();
+		free(result_buf);
+		free(socubuf);
+		result_buf = NULL;
+		result_sz = 0;
+		result_written = 0;
+		return ret;
+	}
+
+	CURLcode cres = curl_easy_perform(hnd);
+    curl_easy_cleanup(hnd);
+	char* newbuf = (char*)realloc(result_buf, result_written + 1);
+	result_buf = newbuf;
+	result_buf[result_written] = 0; //nullbyte to end it as a proper C style string
+	
+	if (cres != CURLE_OK) {
+		printf("Error in:\ncurl\n");
+		socExit();
+		free(result_buf);
+		free(socubuf);
+		result_buf = NULL;
+		result_sz = 0;
+		result_written = 0;
+		return -1;
+	}
+	
+	printf("Looking for asset with name matching:\n%s\n", asset.c_str());
+	std::string assetUrl;
+	json parsedAPI = json::parse(result_buf);
+	if (parsedAPI["assets"].is_array()) {
+		for (auto jsonAsset : parsedAPI["assets"]) {
+			if (jsonAsset.is_object() && jsonAsset["name"].is_string() && jsonAsset["browser_download_url"].is_string()) {
+				std::string assetName = jsonAsset["name"];
+				if (matchPattern(asset, assetName)) {
+					assetUrl = jsonAsset["browser_download_url"];
+					break;
+				}
+			}
+		}
+	}
+    socExit();
+    free(result_buf);
+    free(socubuf);
+	result_buf = NULL;
+	result_sz = 0;
+	result_written = 0;
+	
+	if (assetUrl.empty())
+		ret = DL_ERROR_GIT;
+	else
+		ret = downloadToFile(assetUrl, path);
+	
+	return ret;
+}
