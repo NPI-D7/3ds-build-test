c912095b2be10bef5d008903f8abf2926e163ba9
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-09-24 10:56:51 +0000

    Update common.hpp

--- include/common.hpp
+++ include/common.hpp
@@ -29,15 +29,14 @@
 
 
 extern "C" {
-#endif
+
 #include <malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 #include <stdarg.h>
 
 
-#ifdef __cplusplus
 }
 
 #include <cstdio>
