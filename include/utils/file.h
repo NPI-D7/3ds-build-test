4bf44630fe60f42edbc645c87356c9a0fccb13bc
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-09-24 14:14:57 +0000

    Update and rename file.h to file.hpp

--- include/utils/file.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "common.hpp"
-
-void openFile(Handle* fileHandle, const char * path, bool write);
-void deleteFile(const char * path);
---
+++ include/utils/file.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "common.hpp"
+
+Result openFile(Handle* fileHandle, const char * path, bool write);
+Rrsult deleteFile(const char * path);
