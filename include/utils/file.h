a6d1330466e0258692460639b83bfaa18d7e2204
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-09-28 19:46:37 +0000

    Delete file.h

--- include/utils/file.h
+++
@@ -1,9 +0,0 @@
-
-#include "common.hpp"
-#include <3ds.h>
-
-Result makeDirs(const char * path);
-Result openFile(Handle* fileHandle, const char * path, bool write);
-Result deleteFile(const char * path);
-Result removeDir(const char * path);
-Result removeDirRecursive(const char * path);
