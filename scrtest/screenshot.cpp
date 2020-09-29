dcac986e30f126ed2e3b30241a20d5b4da787677
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-09-29 19:19:40 +0000

    Update screenshot.cpp

--- scrtest/screenshot.cpp
+++ scrtest/screenshot.cpp
@@ -1,1 +1,17 @@
 #include "screenshot.hpp"
+
+#include <3ds.h>
+
+
+static void Screenshot_GenerateFilename(int count, char *file_name) {
+	time_t t = time(0);
+	int day = localtime(&t)->tm_mday;
+	int month = localtime(&t)->tm_mon + 1;
+	int year = localtime(&t)->tm_year + 1900;
+	
+	if (!(FS_DirExists(sdmc_archive, "/3ds/NPI/screenshots/Test/")))
+	FS_RecursiveMakeDir(archive, "/3ds/NPI/music/Test/");
+		
+	sprintf(file_name, "/3ds/NPI/screenshots/Test/Screenshot_%02d%02d%02d-%i.bmp", year, month, day, count);
+}
+
