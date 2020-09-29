66bced6b7ee2b3737040d37b5eaddfc6fb0731a7
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-09-29 16:18:51 +0200

    updating
    CMSG system

--- source/gui/cmsg.c
+++ source/gui/cmsg.c
@@ -1,11 +1,11 @@
 #include "cmsg.h"
 
-CMSG_DrawMSG(float x, float y, float size, Colour colour, const char *text)
+CMSG_DrawMSG(float x, float y, float size, Color, const char *text)
 {
     C2D_Text c2d_text;
 	C2D_TextFontParse(&c2d_text, font, c2d_dynamic_buf, text);
 	C2D_TextOptimize(&c2d_text);
-	C2D_DrawText(&c2d_text, C2D_WithColor, x, y, 0.5f, size, size, colour);
+	C2D_DrawText(&c2d_text, C2D_WithColor, x, y, 0.5f, size, size, Color);
 
 
 }
