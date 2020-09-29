b2ef51b709ead2e26cbb33ee3387d0cbca8de56c
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-09-29 16:44:28 +0000

    Update cmsg.c

--- source/gui/cmsg.c
+++ source/gui/cmsg.c
@@ -1,17 +1,25 @@
 #include <citro2d.h>
 
+#include "common.hpp"
 
 #include <assert.h>
 #include <stdarg.h>
 
 #include "cmsg.h"
 
-void DrawMSG(float x, float y, float size, Colour color, const char *text)
+void DrawMSG(float x, float y, float size, u32 Colour, const char *text, C2D_Font fnt)
 {
     C2D_Text c2d_text;
-	C2D_TextFontParse(&c2d_text, font, c2d_dynamic_buf, text);
-	C2D_TextOptimize(&c2d_text);
-	C2D_DrawText(&c2d_text, C2D_WithColor, x, y, 0.5f, size, size, color);
 
+	if (fnt != nullptr) {
+		C2D_TextFontParse(&c2d_text, fnt, TextBuf, text.c_str());
+	} else {
+		C2D_TextFontParse(&c2d_text, Font, TextBuf, text.c_str());
+	}
+
+	C2D_TextOptimize(&c2d_text);
+
+		
+	
 
 }
