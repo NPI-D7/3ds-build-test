653bfb07be371f01245f26647c49a130ae876f7f
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-09-29 17:08:24 +0200

    Update screenshot.c

--- source/utils/screenshot.c
+++ source/utils/screenshot.c
@@ -109,6 +109,6 @@ void Screenshot_Capture(void) {
 
 	Screenshot_GenerateScreenshot(file_name);
 	num++;
-	DrawMSG(10, 210, 2, CWHITE, "Screenshot Generated:", file_name);
+	DrawMSG(10, 210, 2, CWHITE, "Screenshot Generated:");
        // Msg::DisplayMsg("Screnshot generated...");
 }
