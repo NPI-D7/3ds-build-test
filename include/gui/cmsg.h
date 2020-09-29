d40c1af968a5f9e0d9e17adf305342e7a2f6b7d3
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-09-29 16:26:50 +0200

    Update cmsg.h

--- include/gui/cmsg.h
+++ include/gui/cmsg.h
@@ -1,9 +1,17 @@
+#ifndef _CMSG_H
+#define _CMSG_H
+
+
 #include <3ds.h>
 #include <citro2d.h>
-#include <citro3d.h>
+
 
 typedef u32 Colour;
 extern C2D_Font font;
 extern C2D_TextBuf c2d_static_buf, c2d_dynamic_buf, c2d_size_buf;
 
 CMSG_DrawMSG(float x, float y, float size, Colour color, const char *text);
+
+
+
+#endif
