fc85fa203f2c2cf4e7e9dfb8aa2ab9215e318ca7
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-09-29 06:54:08 +0200

    Update screenshot.c
    updating screenshotsystem

--- source/utils/screenshot.c
+++ source/utils/screenshot.c
@@ -89,7 +89,7 @@ static void Screenshot_GenerateFilename(int count, char *file_name) {
 	int year = localtime(&t)->tm_year + 1900;
 	
 	if (!(FS_DirExists(sdmc_archive, "/3ds/NPI/screenshots/Test/")))
-		FS_MakeDir(sdmc_archive, "/3ds/NPI/screenshots/Test/");
+	FS_RecursiveMakeDir(archive, "/3ds/NPI/music/Test/");
 		
 	sprintf(file_name, "/3ds/NPI/screenshots/Test/Screenshot_%02d%02d%02d-%i.bmp", year, month, day, count);
 }
