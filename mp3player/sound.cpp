4bacdfa6015d4d0e0297b5ad067ec9e632d07337
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-10-02 14:22:42 +0200

    Update sound.cpp

--- mp3player/sound.cpp
+++ mp3player/sound.cpp
@@ -20,23 +20,23 @@
 
 
 
-    size_t currentSong         = 0;
-    std::vector<std::string> bgm;Result mp3::PlayFile()
-    {   
-        STDirectory dir("/3ds/PKSM/songs");  
-        if (dir.good())    {     
-        for (size_t i = 0; i < dir.count(); i++)     
-        { 
-             if (!dir.folder(i))      
-                    {        
-                              auto decoder = Decoder::get("/3ds/PKSM/songs/" + dir.item(i));   
-                                    if (decoder && decoder->good())     
-                                            { 
-           bgm.emplace_back("/3ds/PKSM/songs/" + dir.item(i)); 
-                           }  
-                                       }    
-                                       
-                                       
-                                       
-                                                 return 0;
-                                                 }
+size_t currentSong         = 0;
+std::vector<std::string> bgm;
+Result mp3::PlayFile()
+{
+     STDirectory dir("/3ds/PKSM/songs");
+    if (dir.good())
+    {
+        for (size_t i = 0; i < dir.count(); i++)
+        {
+            if (!dir.folder(i))
+            {
+                auto decoder = Decoder::get("/3ds/PKSM/songs/" + dir.item(i));
+                if (decoder && decoder->good())
+                {
+                    bgm.emplace_back("/3ds/PKSM/songs/" + dir.item(i));
+                }
+            }
+        }
+    }
+}
