f5bdae23eb5443dc48fcbdde277690b950e92184
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-10-02 14:19:49 +0200

    Update sound.cpp

--- mp3player/sound.cpp
+++ mp3player/sound.cpp
@@ -13,38 +13,30 @@
 #include <unordered_map>
 
 
-namespace
-{
 
 
 
 
 
 
-    
-    std::vector<std::string> bgm;
+
     size_t currentSong         = 0;
-
-
-
-    Result mp3::PlayFile(){
-    
-         STDirectory dir("/3ds/PKSM/songs");
-        if (dir.good())
-        {
-             for (size_t i = 0; i < dir.count(); i++)
-             {
-                  if (!dir.folder(i))
-                 {
-                     auto decoder = Decoder::get("/3ds/PKSM/songs/" + dir.item(i));
-                     if (decoder && decoder->good())
-                     {
-                    bgm.emplace_back("/3ds/PKSM/songs/" + dir.item(i));
-                      }
-                  }
-             }
-
-         return 0;
-    
-    }
-}
+    std::vector<std::string> bgm;Result mp3::PlayFile()
+    {   
+        STDirectory dir("/3ds/PKSM/songs");  
+        if (dir.good())    {     
+        for (size_t i = 0; i < dir.count(); i++)     
+        { 
+             if (!dir.folder(i))      
+                    {        
+                              auto decoder = Decoder::get("/3ds/PKSM/songs/" + dir.item(i));   
+                                    if (decoder && decoder->good())     
+                                            { 
+           bgm.emplace_back("/3ds/PKSM/songs/" + dir.item(i)); 
+                           }  
+                                       }    
+                                       
+                                       
+                                       
+                                                 return 0;
+                                                 }
